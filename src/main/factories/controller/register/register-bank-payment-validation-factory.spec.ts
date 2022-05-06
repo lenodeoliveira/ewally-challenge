1e35d974492842086ebb21f9802af3e33c82e1de
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-05-06 05:52:05 -0300

    fix: factories controller

--- src/main/factories/controller/register/register-bank-payment-validation-factory.spec.ts
+++ src/main/factories/controller/register/register-bank-payment-validation-factory.spec.ts
@@ -1,13 +1,13 @@
 
 import { ValidationComposite, RequiredFieldValidation } from '../../../../validation/validators'
-import { makeSignUpController } from './register-bank-payment-controller-factory'
+import { makeRegisterBankPaymentController } from './register-bank-payment-controller-factory'
 import { Validation } from '../../../../presentation/protocols/validation'
 
 jest.mock('../../../../validation/validators/validation-composite')
 
 describe('SignUpValidation Factory', () => {
   test('Should call ValidationComposite with all validation', () => {
-    makeSignUpController()
+    makeRegisterBankPaymentController()
     const validations: Validation[] = []
     for (const field of ['barCode', 'amount', 'expirationDate']) {
       validations.push(new RequiredFieldValidation(field))
