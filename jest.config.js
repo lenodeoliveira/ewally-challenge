bde76911bd486e2bf9082b2a372e85108ddfb810
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-05-09 17:22:05 -0300

    refactor: remov mongodb

--- jest.config.js
+++ jest.config.js
@@ -15,7 +15,6 @@ module.exports = {
     '!<rootDir>/src/**/*-protocols.ts',
     '!**/protocols/**', '!**/test/**'
   ],
-  preset: '@shelf/jest-mongodb',
   transform: {
     '.+\\.ts$': 'ts-jest'
   },
