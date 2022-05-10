a327fde99cb0fe3a9c1a79b8fd1118eae7daf122
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-05-10 10:21:48 -0300

    refactor: remove unused routes

--- src/main/routes/load-bank-payment-routes.ts
+++ src/main/routes/load-bank-payment-routes.ts
@@ -1,13 +1,10 @@
 import { Router } from 'express'
 import { adaptRoute } from '../adapter/express/express-route-adapter'
-// import { makeLoadBankPaymentController } from '../factories/controller/load-bank-payment/load-bank-payment-controller-factory'
 import { LoadBankPaymentController } from '../../presentation/controllers/bank-payment-slip/load-bank-payment/load-bank-payment-controller'
 
 const loadBankPaymentController: LoadBankPaymentController = new LoadBankPaymentController()
 
 export default (router: Router): void => {
-  // eslint-disable-next-line @typescript-eslint/no-misused-promises
-  // router.post('/boleto', adaptRoute(makeRegisterBankPaymentController()))
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.get('/boleto/:code', adaptRoute(loadBankPaymentController))
 }
