e94dfea00ad34d9a7c832985e1045204eae65fdb
Lenodeoliveira <lenodeoliveira@gmail.com>
2022-05-10 07:56:49 -0300

    test: add 500 error test in load-bank-payment-controller

--- src/presentation/controllers/bank-payment-slip/load-bank-payment/load-bank-payment-controller.spec.ts
+++ src/presentation/controllers/bank-payment-slip/load-bank-payment/load-bank-payment-controller.spec.ts
@@ -1,3 +1,4 @@
+import { serverError } from '../../../helpers/http/http-helper'
 import { LoadBankPaymentController } from './load-bank-payment-controller'
 import { HttpRequest } from './load-bank-payment-controller-protocols'
 
@@ -13,5 +14,11 @@ describe('LoadBankPaymentController', () => {
     const loadSpy = jest.spyOn(sut, 'handle')
     await sut.handle(makeFakeRequest().params.code)
     expect(loadSpy).toHaveBeenCalledWith('21290001192110001210904475617405975870000002000')
+  })
+
+  test('Should return 500 if throws', async () => {
+    const sut = new LoadBankPaymentController()
+    const httpResponse = await sut.handle(makeFakeRequest().params.fieldNotExists)
+    expect(httpResponse).toEqual(serverError(new Error()))
   })
 })
